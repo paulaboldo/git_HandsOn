a8bb4520566f20b7cb4f510104618016bdc5bd16
Paula Boldo <paula.boldo.cobo@gmail.com>
2024-01-31 17:10:36 +0100

    task12

--- seqClass.py
+++ seqClass.py
@@ -20,3 +20,10 @@ if re.search('^[ACGTU]+$', args.seq):
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
+if args.motif:
+    args.motif = args.motif.upper()
+    print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq}"... ', end = '')
+    if re.search(args.motif, args.seq):
+        print("FOUND")
+    else:
+        print("NOT FOUND")
